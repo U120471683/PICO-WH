0fae7fa4b4ef4adaaf1271066f2300757855efed
U120471683 <shichanliaw@gmail.com>
2024-09-28 14:12:49 +0800

    sdfgssdd

--- pico/lesson6-7/main1-2-2.py
+++ pico/lesson6-7/main1-2-2.py
@@ -1,20 +1,25 @@
-from machine import Timer,ADC,Pin,PWM
+from machine import Timer,ADC,Pin,PWM,RTC
 
 
 
 adc = ADC(4)
-# pwm =PWM(Pin(15),freq=50)
+pwm =PWM(Pin(15),freq=50)
 conversion_factor = 3.3 / (65535)
+rtc=RTC()
 
 def do_thing(t):
     reading = adc.read_u16() * conversion_factor
     temperature = 27 - (reading - 0.706)/0.001721
+    print(rtc.datetime())
     print(temperature)
+    
+    
+    
     
 def do_thing1(t):
     adc1 = ADC(Pin(26))
     duty = adc1.read_u16()
-   # pwm.duty_u16(duty)
+    pwm.duty_u16(duty)
     
     print(f'可變電阻:{round(duty/65535*10)}')
 
