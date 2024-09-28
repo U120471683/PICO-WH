1cbe833963a0e3f844bfb8fecaacfc454f22397f
U120471683 <shichanliaw@gmail.com>
2024-09-28 11:48:03 +0800

    fdsfsdf

--- pico/lesson6-7/main1-2-1.py
+++ pico/lesson6-7/main1-2-1.py
@@ -9,11 +9,14 @@ def do_thing(t):
     reading = adc.read_u16() * conversion_factor
     temperature = 27 - (reading - 0.706)/0.001721
     print(temperature)
-
+    
 def do_thing1(t):
-    adc1=ADC(Pin(26))
-    duty = adc1.read_u16() * conversion_factor
-    print(f'可變電阻值:{duty}')
+    adc1 = ADC(Pin(26))
+    duty = adc1.read_u16()
+    
+    print(f'可變電阻:{round(duty/65535*10)}')
 
-t1=Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
-t2=Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
+
+
+t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
