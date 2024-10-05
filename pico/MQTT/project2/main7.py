e1b82f963310d17f925f1a6833a281afe5bf34db
U120471683 <shichanliaw@gmail.com>
2024-10-05 14:27:56 +0800

    dgfgrtgf

--- pico/MQTT/project2/main7.py
+++ pico/MQTT/project2/main7.py
@@ -1,22 +1,56 @@
-import network
-import time
+#! usr/bin/micropython
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect('A590301', 'A590301AA')
+'''
+led->gpio15
+光敏電阻 -> gpio28
+可變電阻 -> gpio26
+內建溫度sensor -> adc最後1pin,共5pin
+'''
 
-max_wait = 10
-while max_wait > 0:
-    if wlan.status() < 0 or wlan.status() >= 3:
-        break
-    max_wait -= 1
-    print('waiting for connection...')
-    time.sleep(1)
+from machine import Timer,ADC,Pin,PWM,RTC
+import tools
 
-# Handle connection error
-if wlan.status() != 3:    
-    raise RuntimeError('network connection failed')
-else:
-    print('connected')
-    status = wlan.ifconfig()
-    print( 'ip = ' + status[0])
+
+def do_thing(t):
+    '''
+    :param t:Timer的實體
+    負責偵測溫度和光線
+    每2秒執行1次
+    '''
+    conversion_factor = 3.3 / (65535)
+    reading = adc.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721  
+    print(f'溫度:{temperature}')
+    adc_value = adc_light.read_u16()
+    print(f'光線:{adc_value}')
+    
+    
+def do_thing1(t):
+    '''
+    :param t:Timer的實體
+    負責可變電阻和改變led的亮度
+    '''    
+    
+    duty = adc1.read_u16()
+    pwm.duty_u16(duty)    
+    print(f'可變電阻:{round(duty/65535*10)}')
+    
+
+def main():
+    try:
+        tools.connect()
+    except RuntimeError as e:
+        print(e)
+    except Exception:
+        print('不知明的錯誤')
+    else:
+        t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+        t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
+
+if __name__ == '__main__':
+    adc = ADC(4) #內建溫度
+    adc1 = ADC(Pin(26)) #可變電阻
+    adc_light = ADC(Pin(28)) #光敏電阻
+    pwm = PWM(Pin(15),freq=50) #pwm led
+    
+    main()
