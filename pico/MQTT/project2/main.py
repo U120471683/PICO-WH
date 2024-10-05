4103bc968d2da7f7325b7802a1acb6b305a0c293
U120471683 <shichanliaw@gmail.com>
2024-10-05 10:23:45 +0800

    sdfsdf

--- pico/MQTT/project2/main.py
+++ pico/MQTT/project2/main.py
@@ -1,38 +1,30 @@
+import tools
 from machine import Timer,ADC,Pin,PWM,RTC
+
+tools.connect()
+
+
+adc = ADC(4)
+pwm = PWM(Pin(15),freq=50)
+conversion_factor = 3.3 / (65535)
+rtc = RTC()
+
 def do_thing(t):
-    '''
-    處理溫度和光線
-    '''
     reading = adc.read_u16() * conversion_factor
     temperature = 27 - (reading - 0.706)/0.001721
-    print(f'溫度:{temperature}')
-    light_value = adc_light.read_u16()
-    print(f'光線:{light_value}')
-    
+    year,month,day,weekly,hours,minuse,seconds,info = rtc.datetime()
+    datetime_str = f"{year}-{month}-{day} {hours}:{minuse}:{seconds}"
+    print(datetime_str)
+    print(temperature)
     
 def do_thing1(t):
-    '''
-    處理可變電阻
-    '''
     adc1 = ADC(Pin(26))
     duty = adc1.read_u16()
-    pwm.duty_u16(duty)    
+    pwm.duty_u16(duty)
+    
     print(f'可變電阻:{round(duty/65535*10)}')
-    
-def main():
-    t1 = Timer(period=1000, mode=Timer.PERIODIC, callback=do_thing)
-    t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
-    
-if __name__ == "__main__":
-    #pico_連結電腦時的寫法,要用connect
-    try:
-        tools.connect()
-    except RuntimeError as e:
-        print(f"{{e}")
-    else:
-        #sensor setup
-        adc = ADC(4) #內建溫度感測器
-        adc_light = ADC(Pin(28)) #光線感測器
-        pwm = PWM(Pin(15),freq=50) #可變電阻
-        conversion_factor = 3.3 / (65535) #電壓轉換率    
-        main()
+
+
+
+t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
