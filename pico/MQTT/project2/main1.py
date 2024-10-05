221dbf90ec4769062c705378d975e23d35cddc03
U120471683 <shichanliaw@gmail.com>
2024-10-05 11:37:52 +0800

    dfgfsdfsd

---
+++ pico/MQTT/project2/main1.py
@@ -0,0 +1,34 @@
+from machine import Timer,ADC,Pin,PWM,RTC
+
+adc = ADC(4)
+pwm = PWM(Pin(15),freq=50)
+conversion_factor = 3.3 / (65535)
+rtc = RTC()
+
+def do_thing(t):
+    reading = adc.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    year,month,day,weekly,hours,minuse,seconds,info = rtc.datetime()
+    datetime_str = f"{year}-{month}-{day} {hours}:{minuse}:{seconds}"
+    print(datetime_str)
+    print(temperature)
+    
+def do_thing1(t):
+    adc1 = ADC(Pin(26))
+    duty = adc1.read_u16()
+    pwm.duty_u16(duty)
+    
+    print(f'可變電阻:{round(duty/65535*10)}')
+
+def do_thing2(t):
+    adc2 = ADC(Pin(28))
+    duty2 = adc2.read_u16()
+    pwm.duty_u16(duty2)
+    
+    print(f'光敏電阻:{round(duty2/65535*10)}')
+
+
+
+t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
+t3 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing2)
