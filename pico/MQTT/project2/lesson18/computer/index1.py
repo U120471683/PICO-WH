6a1ddab8873c68d90dc6cef357c9da16c81e5721
U120471683 <shichanliaw@gmail.com>
2024-10-26 09:56:16 +0800

    sdsdfsdfsd

---
+++ pico/MQTT/project2/lesson18/computer/index1.py
@@ -0,0 +1,36 @@
+import paho.mqtt.client as mqtt
+
+def record():
+    pass
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    #連線bloker成功時,只會執行一次
+    client.subscribe("SA-59/#")
+
+def on_message(client, userdata, msg):
+    global led_origin_value
+    topic = msg.topic
+    value = msg.payload.decode()
+    if topic == 'SA-59/LED_LEVEL':
+        led_value = int(value)
+        if led_value != led_origin_value:
+            led_origin_value = led_value
+            print(f'led_value:{led_value}')
+    #print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
+
+def main():
+    client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+    # 設定用戶名和密碼
+    username = "pi"  # 替換為您的用戶名
+    password = "raspberry"  # 替換為您的密碼
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.on_message = on_message 
+    #client.connect("192.168.0.252", 1883, 60)
+    client.connect("192.168.1.128", 1883, 60)
+    client.loop_forever()
+
+
+if __name__ == "__main__":
+    led_origin_value = 0 
+    main()
